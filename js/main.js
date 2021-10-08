854f4f9bd7a01702df6576bfcce5c0a50b7de14b
premiuncarrotshop <benqu94@naver.com>
2021-10-08 16:56:24 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgesEl = document.querySelector('header .badges');
 const scrollEl = document.querySelector('#to-top');
 window.addEventListener('scroll', _.throttle(function() {
@@ -136,5 +121,3 @@ spyEls.forEach(function(spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const year = document.querySelector('.this-year');
-year.textContent = new Date().getFullYear();
